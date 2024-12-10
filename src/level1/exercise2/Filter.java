25cc742178027bab2a958216d101cb0adda3314d
Enric <reiben815@gmail.com>
2024-12-10 13:06:23 +0100

    Code updated based on feedback

--- src/level1/exercise2/Filter.java
+++ src/level1/exercise2/Filter.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Filter {
     public static List<String> filterStringsWithO(List<String> strings) {
         return strings.stream()
-                .filter(s -> (s.contains("o") || s.contains("O")) && s.length() > 5)
+                .filter(s -> s.toLowerCase().contains("o") && s.length() > 5)
                 .collect(Collectors.toList());
     }
 }
